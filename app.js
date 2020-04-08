df3cc3cb334511aec4a7b67310da6f9bc25ad46c
maxjmorris <morrm046@gmail.com>
2020-04-08 16:05:45 +0100

    Added function to add data to the mongodb database

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const path = require('path')
 //Init app
 const app = express();
-var mongoose = require('mongoose')
-
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser');
 
 mongoose.connect('mongodb://localhost/myapp_db', {useNewUrlParser : true});
 
@@ -20,12 +20,15 @@ let users = require('./models/users')
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine', 'pug')
 
+
+//Body parser middleware
+//parse application
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 //home route
 app.get('/', function(req,res)
 {
     res.render('index');
-
-
 });
 //Start server
 app.listen(3000,function()
@@ -37,3 +40,24 @@ function handleRedirect(req, res){
   const targetUrl = targetBaseUrl + req.originalUrl;
   res.redirect(targetUrl);
 }
+
+//Add submit to POST route
+app.post('/',function(req,res){
+  let user = new Users();
+  user.firstname = req.body.firstname;
+  user.lastname = req.body.lastname;
+  user.location = req.body.location;
+
+  user.save(function()
+{
+  if(err){
+    console.log(err);
+    return
+  }
+  else {
+    {
+      res.redirect('/')
+    }
+  }
+})
+})
