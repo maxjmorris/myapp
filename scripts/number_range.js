0b91913996b5adda79eb245e2de5ddb00285e7c7
Max Morris <p15213515@my365.dmu.ac.uk>
2020-04-24 16:02:39 +0100

    Changed script into a function

--- scripts/number_range.js
+++ scripts/number_range.js
@@ -1,9 +1,16 @@
-let number = window.prompt("Please enter a number");
 
-
-if (number >= 50 && number <= 200)
+function numberRange(number)
 {
-  return "Number is in range";
-} else {
-  return "Number not in range";
+  if (number >= 50 && number <= 200)
+  {
+    return "Number is in range";
+  } else {
+    return "Number not in range";
+  }
 }
+
+console.log(numberRange(50));
+console.log(numberRange(201));
+console.log(numberRange(49));
+console.log(numberRange(100));
+console.log(numberRange(172));
