49f6ece0414dbcecd5470194dfb5159c0707ca18
maxjmorris <morrm046@gmail.com>
2020-04-30 13:38:25 +0100

    made copy of number_range.py for now

---
+++ number_range.py
@@ -0,0 +1,15 @@
+import sys
+
+
+def number_range(number):
+    if int(number) >= 50 and int(number) <= 200:
+        return "Number is valid"
+    else:
+        return "Number is invalid"
+
+
+#print(number_range(50))
+
+print(number_range(sys.argv[1]))
+
+
