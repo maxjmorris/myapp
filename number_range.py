b8b2f3e5954a54f960b68b832c1e61f7c9136065
Max Morris <p15213515@my365.dmu.ac.uk>
2020-05-01 11:56:29 +0100

    Added some more code to my python script

--- number_range.py
+++ number_range.py
@@ -3,16 +3,23 @@ import sys
 
 def number_range(number):
     if isinstance(number, int):
-        if int(number) >= 50 and int(number) <= 200:
-            return "Number is valid"
-        else:
-            return "Number is invalid"
+        if number < 50:
+            return "The number you put in " + str(number) + " is too low"
+        elif number > 200:
+            return "The number you put in " + str(number) + " is too high"
+        else:                        
+            return "The number you put in was " + str(number) + " is just right"
     else:
-        return "Please enter a number"
+        return "Please enter a number what you entered " + str(number) + " is not a number"
 
 
-#print(number_range(50))
+print(number_range(30))
+print(number_range(50))
+print(number_range(201))
+print(number_range(70))
+print(number_range("MAX"))
 
-print(number_range(sys.argv[1]))
+
+
 
 
