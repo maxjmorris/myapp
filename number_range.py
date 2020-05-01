24386cd0d6dc294f67c381b23e21b59b36b56eee
Max Morris <p15213515@my365.dmu.ac.uk>
2020-05-01 12:01:38 +0100

    some of the code is incorrect issue with data types

--- number_range.py
+++ number_range.py
@@ -2,15 +2,12 @@ import sys
 
 
 def number_range(number):
-    if isinstance(number, int):
         if number < 50:
             return "The number you put in " + str(number) + " is too low"
         elif number > 200:
             return "The number you put in " + str(number) + " is too high"
         else:                        
             return "The number you put in was " + str(number) + " is just right"
-    else:
-        return "Please enter a number what you entered " + str(number) + " is not a number"
 
 
 print(number_range(sys.argv[1]))
